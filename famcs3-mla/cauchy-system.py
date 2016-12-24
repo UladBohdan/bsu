f334e84a1d95d22608d3c3073bcbe52f86d70b53
uladbohdan <vlbohdan@gmail.com>
2016-12-24 20:32:55 +0300

    MLA: Cauchy for equations and systems

--- famcs3-mla/cauchy-system.py
+++ famcs3-mla/cauchy-system.py
@@ -1,6 +1,7 @@
 import math
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
+# Defining the problem (system of differential equations).
 dy_dx = lambda x, y, z: math.e**(-(y*y + z*z)) + 2 * x
 dz_dx = lambda x, y, z: 2 * y * y + z
 x_range = [0., 0.5]
@@ -11,19 +12,27 @@ z0 = 1.
 f = dy_dx
 g = dz_dx
 
+# Defining the grid to make the calculations.
+n = 10
+h = (x_range[1] - x_range[0]) / n
+grid = []
+for i in range(0, n):
+    grid.append(x_range[0] + i * h)
+
+# Constants for algorithms.
 EPS = 1e-5
 MAX_ITERATIONS = 100
 
-def euler_explicit_system(grid):
+def euler_explicit_system():
     yi = [y0]
     zi = [z0]
     for i in range(1, len(grid)):
         step = grid[i] - grid[i-1]
         yi.append(yi[i-1] + step * f(grid[i-1], yi[i-1], zi[i-1]))
-        zi.append(zi[i-1] + step * g(grid[i-1], zi[i-1], zi[i-1]))
+        zi.append(zi[i-1] + step * g(grid[i-1], yi[i-1], zi[i-1]))
     return (yi, zi)
 
-def euler_implicit_system(grid):
+def euler_implicit_system():
     yi = [y0]
     zi = [z0]
     for i in range(1, len(grid)):
@@ -31,7 +40,6 @@ def euler_implicit_system(grid):
         result = simple_iterations(yi[i-1], zi[i-1], step, f, g, grid[i], yi[i-1], zi[i-1])
         yi.append(result[0])
         zi.append(result[1])
-        #zi.append(simple_iterations(zi[i-1], step, g, grid[i], yi[i-1], zi[i-1]))
     return (yi, zi)
 
 def simple_iterations(base_y, base_z, step, f, g, x, y0, z0):
@@ -40,47 +48,94 @@ def simple_iterations(base_y, base_z, step, f, g, x, y0, z0):
     iter = MAX_ITERATIONS
     while iter > 0:
         y_new = base_y + step * f(x, y_prev, z_prev)
-        iter -= 1
-        if abs(y_prev - y_new) < EPS:
-            break
-        y_prev = y_new
-    if iter == 0:
-        print "simple iterations: no convergence"
-    iter = MAX_ITERATIONS
-    while iter > 0:
         z_new = base_z + step * g(x, y_prev, z_prev)
         iter -= 1
-        if abs(z_prev - z_new) < EPS:
-            break
+        if abs(y_prev - y_new) < EPS and abs(z_prev - z_new) < EPS:
+            return (y_prev, z_prev)
+        y_prev = y_new
         z_prev = z_new
-    if iter == 0:
-        print "simple iterations: no convergence"
-    return (y_prev, z_prev)
-
-def predictor_corrector_system(grid):
+    print "simple iterations: no convergence"
     return
 
+def predictor_corrector_system():
+    yi3 = [y0]
+    zi3 = [z0]
+    for i in range(1, len(grid)):
+        step = grid[i] - grid[i-1]
+
+        y2 = yi3[i-1] + step * f(grid[i-1], yi3[i-1], zi3[i-1])
+        y3 = yi3[i-1] + step * (f(grid[i-1], yi3[i-1], zi3[i-1]) + f(grid[i], y2, zi3[i-1])) / 2.
+        yi3.append(y3)
+
+        z2 = zi3[i-1] + step * g(grid[i-1], yi3[i-1], zi3[i-1])
+        z3 = zi3[i-1] + step * (g(grid[i-1], yi3[i-1], zi3[i-1]) + g(grid[i], yi3[i-1], z2)) / 2.
+        zi3.append(z3)
+    return (yi3, zi3)
+
+def runge_kutta_system():
+    yi = [y0]
+    zi = [z0]
+    a0 = 0.25
+    a1 = 0.
+    a2 = 0.75
+    for i in range(1, len(grid)):
+        step = grid[i] - grid[i-1]
+
+        fi_0_y = step * f(grid[i-1], yi[i-1], zi[i-1])
+        fi_0_z = step * g(grid[i-1], yi[i-1], zi[i-1])
+        fi_1_y = step * f(grid[i-1] + step/3., yi[i-1] + fi_0_y/3., zi[i-1] + fi_0_z/3.)
+        fi_1_z = step * g(grid[i-1] + step/3., yi[i-1] + fi_0_y/3., zi[i-1] + fi_0_z/3.)
+        fi_2_y = step * f(grid[i-1] + 2.*step/3., yi[i-1] + 2.*fi_1_y/3., zi[i-1] + 2.*fi_1_z/3.)
+        fi_2_z = step * g(grid[i-1] + 2.*step/3., yi[i-1] + 2.*fi_1_y/3., zi[i-1] + 2.*fi_1_z/3.)
+
+        y_new = yi[i-1] + a0 * fi_0_y + a1 * fi_1_y + a2 * fi_2_y
+        yi.append(y_new)
+
+        z_new = zi[i-1] + a0 * fi_0_z + a1 * fi_1_z + a2 * fi_2_z
+        zi.append(z_new)
+
+    return (yi, zi)
+
+def inter_adams_system(y_approx, z_approx):
+    yi = y_approx
+    zi = z_approx
+    a0 = 5./12
+    a1 = 8./12
+    a2 = -1./12
+    for i in range(2, len(grid)):
+        step = grid[i] - grid[i-1]
+        f_local = lambda x, y, z: a0 * f(x, y, z) + a1 * f(grid[i-1], yi[i-1], zi[i-1]) + a2 * f(grid[i-2], yi[i-2], zi[i-2])
+        g_local = lambda x, y, z: a0 * g(x, y, z) + a1 * g(grid[i-1], yi[i-1], zi[i-1]) + a2 * g(grid[i-2], yi[i-2], zi[i-2])
+        result = simple_iterations(yi[i-1], zi[i-1], step, f_local, g_local, grid[i], yi[i-1], zi[i-1])
+        yi.append(result[0])
+        zi.append(result[1])
+    return (yi, zi)
+
 def output_system(label, x, y, z):
+    print "---------------------------------"
     print label
     print "Y:"
     for i in range(0, len(x)):
         print 'y({0}) = {1}'.format(x[i], y[i])
-    print "---------------------------------"
     print "Z:"
     for i in range(0, len(x)):
         print 'z({0}) = {1}'.format(x[i], z[i])
+    print "---------------------------------"
 
 def run_methods():
-    n = 10
-    h = (x_range[1] - x_range[0]) / n
-    grid = []
-    for i in range(0, n):
-        grid.append(x_range[0] + i * h)
+    y_euler_explicit, z_euler_explicit = euler_explicit_system()
+    output_system("Explicit Euler for system:", grid, y_euler_explicit, z_euler_explicit)
 
-    y_euler_explicit, z_euler_explicit = euler_explicit_system(grid)
-    output_system("Explicit Euler for system", grid, y_euler_explicit, z_euler_explicit)
+    y_euler_implicit, z_euler_implicit = euler_implicit_system()
+    output_system("Implicit Euler for system:", grid, y_euler_implicit, z_euler_implicit)
 
-    y_euler_implicit, z_euler_implicit = euler_implicit_system(grid)
-    output_system("Implicit Euler for system", grid, y_euler_implicit, z_euler_implicit)
+    y_predictor_corrector, z_predictor_corrector = predictor_corrector_system()
+    output_system("Predictor-corrector for system:", grid, y_predictor_corrector, z_predictor_corrector)
+
+    y_runge_kutta, z_runge_kutta = runge_kutta_system()
+    output_system("Runge-Kutta for system:", grid, y_runge_kutta, z_runge_kutta)
+
+    y_inter_adams, z_inter_adams = inter_adams_system(y_predictor_corrector[:2], z_predictor_corrector[:2])
+    output_system("Interpolative Adams for system:", grid, y_inter_adams, z_inter_adams)
 
 run_methods()
--- famcs3-mla/cauchy.py
+++ famcs3-mla/cauchy.py
@@ -1,13 +1,26 @@
 import math
 import matplotlib.pyplot as plt
 
+# Defining the Cauchy problem for equation.
 du_dx = lambda x, u: - u * math.cos(x) + math.sin(x) * math.cos(x)
 x_range = [0., 1.]
 x0 = 0
 u0 = -1
 
+f = du_dx
+
+# Defining a grid to make calculations on.
+n = 10
+h = (x_range[1] - x_range[0]) / n
+grid = []
+for i in range(0, n):
+    grid.append(x_range[0] + i * h)
+
+# Exact solution of the problem.
 exact_solution = lambda x: -1 + math.sin(x)
 
+# Some pre-calculations. k is for highest degree of derivations.
+k = 3
 d_dx = [
     lambda x, u: u0,
     lambda x, u: du_dx(x, u),
@@ -15,10 +28,11 @@ d_dx = [
     lambda x, u: u * math.cos(x) - 2 * math.sin(2 * x),
 ]
 
+# Constants for algorithms.
 EPS = 1e-5
 MAX_NEWTON_ITERATIONS = 100
 
-def series(grid, k):
+def series():
     print "Simple Taylor series approximation coefficients:"
     for i in range(0, k+1):
         print 'x^{0} * {1}'.format(i, d_dx[i](x0, u0) / math.factorial(i))
@@ -33,38 +47,36 @@ def series(grid, k):
         yi.append(y)
     return yi
 
-def euler_explicit(grid):
+def euler_explicit():
     yi = [u0]
-    f = du_dx
     for i in range(1, len(grid)):
         x = grid[i]
         step = grid[i] - grid[i-1]
         yi.append( yi[-1] + step*f(x,yi[-1]) )
     return yi
 
-def euler_implicit(grid):
+def euler_implicit():
     yi = [u0]
-    f = du_dx
     for i in range(1, len(grid)):
         step = grid[i] - grid[i-1]
-        yi.append(newton_iterations(yi[i-1], step, f, grid[i], yi[i-1]))
+        yi.append(newton_iterations(step, grid[i], yi[i-1]))
     return yi
 
-def newton_iterations(base, step, f, x, y0):
+def newton_iterations(step, x, y0):
     y_prev = y0
     iter = MAX_NEWTON_ITERATIONS
+    df_dy = lambda x, u: - math.cos(x)
     while iter > 0:
-        y_new = base + step * f(x, y_prev)
+        y_new = y_prev - (y_prev - y0 - step * f(x, y_prev)) / (1. - step * df_dy(x, y_prev))
         iter -= 1
         if abs(y_prev - y_new) < EPS:
             return y_new
         y_prev = y_new
     print "newton iterations: no convergence"
-    return y0
+    return
 
-def predictor_corrector(grid):
+def predictor_corrector():
     yi4 = [u0]
-    f = du_dx
     for i in range(1, len(grid)):
         step = grid[i] - grid[i-1]
         y_n4_2 = yi4[i-1] + step * f(grid[i-1], yi4[i-1]) / 4.
@@ -74,23 +86,20 @@ def predictor_corrector(grid):
         yi4.append(y4)
     return yi4
 
-def runge_kutta(grid):
+def runge_kutta():
     yi = [u0]
-    f = du_dx
     a0 = 0.
     a1 = 1.
     for i in range(1, len(grid)):
         step = grid[i] - grid[i-1]
-        tetta0 = step * f(grid[i-1], yi[i-1])
-        tetta1 = step * f(grid[i-1] + step/2., yi[i-1] + tetta0/2.)
-        y_new = yi[i-1] + a0 * tetta0 + a1 * tetta1
+        fi0 = step * f(grid[i-1], yi[i-1])
+        fi1 = step * f(grid[i-1] + step/2., yi[i-1] + fi0/2.)
+        y_new = yi[i-1] + a0 * fi0 + a1 * fi1
         yi.append(y_new)
     return yi
 
-def extra_adams(grid, approx):
+def extra_adams(approx):
     yi = approx
-    f = du_dx
-    print "Approx len: ", len(yi)
     for i in range(3, len(grid)):
         step = grid[i] - grid[i-1]
         y_new = yi[i-1] + step * (23 * f(grid[i-1], yi[i-1]) - 15 * f(grid[i-2], yi[i-2]) + 5 * f(grid[i-3], yi[i-3])) / 12.
@@ -103,39 +112,33 @@ def output(header, x, y):
         print 'y({0}) = {1}'.format(x[i], y[i])
 
 def run_methods():
-    k = 3
-    n = 10
-    h = (x_range[1] - x_range[0]) / n
-    grid = []
-    for i in range(0, n):
-        grid.append(x_range[0] + i * h)
-
-    y0 = []
+    y_exact = []
     for x in grid:
-        y0.append(exact_solution(x))
-    plt.plot(grid, y0, color='red')
+        y_exact.append(exact_solution(x))
+    output("Exact solution: ", grid, y_exact)
+    plt.plot(grid, y_exact, color='red')
 
-    y_series = series(grid, k)
+    y_series = series()
     output("Series method:", grid, y_series)
     plt.plot(grid, y_series, color='blue')
 
-    y_euler_explicit  = euler_explicit(grid)
+    y_euler_explicit  = euler_explicit()
     output("Explicit Euler:", grid, y_euler_explicit)
     plt.plot(grid, y_euler_explicit, color='green')
 
-    y_euler_implicit = euler_implicit(grid)
+    y_euler_implicit = euler_implicit()
     output("Implicit Euler: ", grid, y_euler_implicit)
     plt.plot(grid, y_euler_implicit, color="black")
 
-    y_predictor_corrector = predictor_corrector(grid)
+    y_predictor_corrector = predictor_corrector()
     output("Predictor-corrector:", grid, y_predictor_corrector)
     plt.plot(grid, y_predictor_corrector, color="brown")
 
-    y_runge_kutta = runge_kutta(grid)
+    y_runge_kutta = runge_kutta()
     output("Runge-Kutta:", grid, y_runge_kutta)
     plt.plot(grid, y_runge_kutta, color="yellow")
 
-    y_extra_adams = extra_adams(grid, y_predictor_corrector[:3])
+    y_extra_adams = extra_adams(y_predictor_corrector[:3])
     output("Extra Adams:", grid, y_extra_adams)
     plt.plot(grid, y_extra_adams, color="red")
 
