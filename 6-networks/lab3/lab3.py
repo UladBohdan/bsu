b90f7ae9659cec0dd839fd7178a1b3570556c893
uladbohdan <vlbohdan@gmail.com>
2017-03-21 15:41:52 +0300

    [6-networks] base for lab3

---
+++ 6-networks/lab3/lab3.py
@@ -0,0 +1,17 @@
+# Task 1.
+IP1 = "32.123.211.78"
+# Task 2.
+IP_list = [
+    "255.255.1.0",
+    "128.61.0.1",
+]
+# Task 3.
+IP3 = "171.81.144.120"
+mask3 = "255.255.255.0"
+# Task 4.
+IP4 = "196.105.157.223/23"
+mask4 = "185.206.219.63/29"
+
+# The algorithm is proposed in the task of the problem.
+def IP_to_binary(IP):
+    return "0001"
