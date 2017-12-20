56008a3e2a0bb02cc5d296b012561cdf7a1dab4c
Uladzislau Bohdan <vlbohdan@gmail.com>
2017-12-21 01:10:10 +0300

    [7-parallel-computations] fixing run.sh comments

--- 7-parallel-computations/lab3-gauss/run.sh
+++ 7-parallel-computations/lab3-gauss/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 mpicc gauss.c || exit 1
-# 10 stangs for matrix size (N).
+# $1 is for number of processes, $2 - matrix size, 5 - R1 (sample value), 10 - R3 (sample)
 mpirun -np $1 a.out $2 5 10
